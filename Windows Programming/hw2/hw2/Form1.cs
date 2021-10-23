a5e166114ab11eff2e87e427eeaef80f1885b80e
ZackChen1140 <76466858+ZackChen1140@users.noreply.github.com>
2021-10-23 17:44:06 +0800

    Add files via upload

---
+++ Windows Programming/hw2/hw2/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hw2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Brush myBrush = new SolidBrush(Color.Black);
+            Graphics g1 = this.CreateGraphics();
+            g1.DrawRectangle(new Pen(myBrush), 0, 30, 60, 60);
+            g1.DrawRectangle(new Pen(myBrush), 60, 30, 60, 60);
+            g1.DrawRectangle(new Pen(myBrush), 120, 30, 60, 60);
+            g1.DrawRectangle(new Pen(myBrush), 0, 90, 60, 60);
+            g1.DrawRectangle(new Pen(myBrush), 60, 90, 60, 60);
+            g1.DrawRectangle(new Pen(myBrush), 120, 90, 60, 60);
+            g1.DrawRectangle(new Pen(myBrush), 0, 150, 60, 60);
+            g1.DrawRectangle(new Pen(myBrush), 60, 150, 60, 60);
+            g1.DrawRectangle(new Pen(myBrush), 120, 150, 60, 60);
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+        int I = 0;
+        int II = 0;
+        int III = 0;
+        int IV = 0;
+        int V = 0;
+        int VI = 0;
+        int VII = 0;
+        int VIII = 0;
+        int IX = 0;
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Brush myBrush = new SolidBrush(Color.Blue), myBrush2 = new SolidBrush(Color.Red);
+            Graphics g2 = this.CreateGraphics();
+            if (e.Location.Y > 30 && e.Location.Y < 90)
+            {
+                if (e.Location.X > 0 && e.Location.X < 60)
+                {
+                    if (I == 0)
+                    {
+                        g2.DrawEllipse(new Pen(myBrush), 5, 35, 50, 50);
+                        I = 1;
+                    }
+                }
+                else if (e.Location.X > 60 && e.Location.X < 120)
+                {
+                    if (II == 0)
+                    {
+                        g2.DrawEllipse(new Pen(myBrush), 65, 35, 50, 50);
+                        II = 1;
+                    }
+                }
+                else if (e.Location.X > 120 && e.Location.X < 180)
+                {
+                    if (III == 0)
+                    {
+                        g2.DrawEllipse(new Pen(myBrush), 125, 35, 50, 50);
+                        III = 1;
+                    }
+                }
+            }
+            else if (e.Location.Y > 90 && e.Location.Y < 150)
+            {
+                if (e.Location.X > 0 && e.Location.X < 60)
+                {
+                    if (IV == 0)
+                    {
+                        g2.DrawEllipse(new Pen(myBrush), 5, 95, 50, 50);
+                        IV = 1;
+                    }
+                }
+                else if (e.Location.X > 60 && e.Location.X < 120)
+                {
+                    if (V == 0)
+                    {
+                        g2.DrawEllipse(new Pen(myBrush), 65, 95, 50, 50);
+                        V = 1;
+                    }
+                }
+                else if (e.Location.X > 120 && e.Location.X < 180)
+                {
+                    if (VI == 0)
+                    {
+                        g2.DrawEllipse(new Pen(myBrush), 125, 95, 50, 50);
+                        VI = 1;
+                    }
+                }
+            }
+            else if (e.Location.Y > 150 && e.Location.Y < 210)
+            {
+                if (e.Location.X > 0 && e.Location.X < 60)
+                {
+                    if (VII == 0)
+                    {
+                        g2.DrawEllipse(new Pen(myBrush), 5, 155, 50, 50);
+                        VII = 1;
+                    }
+                }
+                else if (e.Location.X > 60 && e.Location.X < 120)
+                {
+                    if (VIII == 0)
+                    {
+                        g2.DrawEllipse(new Pen(myBrush), 65, 155, 50, 50);
+                        VIII = 1;
+                    }
+                }
+                else if (e.Location.X > 120 && e.Location.X < 180)
+                {
+                    if (IX == 0)
+                    {
+                        g2.DrawEllipse(new Pen(myBrush), 125, 155, 50, 50);
+                        IX = 1;
+                    }
+                }
+            }
+            Point[] pt1 = new Point[2];
+            if (I == 1 && II == 1 && III == 1)
+            {
+                pt1[0].X = 5; pt1[0].Y = 60;
+                pt1[1].X = 175; pt1[1].Y = 60;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You win!";
+            }
+            if (IV == 1 && V == 1 && VI == 1)
+            {
+                pt1[0].X = 5; pt1[0].Y = 120;
+                pt1[1].X = 175; pt1[1].Y = 120;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You win!";
+            }
+            if (VII == 1 && VIII == 1 && IX == 1)
+            {
+                pt1[0].X = 5; pt1[0].Y = 180;
+                pt1[1].X = 175; pt1[1].Y = 180;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You win!";
+            }
+            if (I == 1&&IV==1&&VII==1)
+            {
+                pt1[0].X = 30; pt1[0].Y = 35;
+                pt1[1].X = 30; pt1[1].Y = 205;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You win!";
+            }
+            if(II==1&&V==1&&VIII==1)
+            {
+                pt1[0].X = 90; pt1[0].Y = 35;
+                pt1[1].X = 90; pt1[1].Y = 205;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You win!";
+            }
+            if(III==1&&VI==1&&IX==1)
+            {
+                pt1[0].X = 150; pt1[0].Y = 35;
+                pt1[1].X = 150; pt1[1].Y = 205;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You win!";
+            }
+            if(I==1&&V==1&&IX==1)
+            {
+                pt1[0].X = 175; pt1[0].Y = 205;
+                pt1[1].X = 5; pt1[1].Y = 35;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You win!";
+            }
+            if(III==1&&V==1&&VII==1)
+            {
+                pt1[0].X = 175; pt1[0].Y = 35;
+                pt1[1].X = 5; pt1[1].Y = 205;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You win!";
+            }
+            Point[] pt2 = new Point[2], pt2II = new Point[2];
+            if (I==0)
+            {
+                I = 2;
+                pt2[0].X = 5 ;pt2[0].Y = 35; pt2[1].X = 55; pt2[1].Y = 85;
+                pt2II[0].X = 5; pt2II[0].Y = 85; pt2II[1].X = 55; pt2II[1].Y = 35;
+                g2.DrawLine(new Pen(myBrush), pt2[0], pt2[1]);
+                g2.DrawLine(new Pen(myBrush), pt2II[0], pt2II[1]);
+            }
+            else if(II==0)
+            {
+                II = 2;
+                pt2[0].X = 65; pt2[0].Y = 35; pt2[1].X = 115; pt2[1].Y = 85;
+                pt2II[0].X = 65; pt2II[0].Y = 85; pt2II[1].X = 115; pt2II[1].Y = 35;
+                g2.DrawLine(new Pen(myBrush), pt2[0], pt2[1]);
+                g2.DrawLine(new Pen(myBrush), pt2II[0], pt2II[1]);
+            }
+            else if (III == 0)
+            {
+                III = 2;
+                pt2[0].X = 125; pt2[0].Y = 35; pt2[1].X = 175; pt2[1].Y = 85;
+                pt2II[0].X = 125; pt2II[0].Y = 85; pt2II[1].X = 175; pt2II[1].Y = 35;
+                g2.DrawLine(new Pen(myBrush), pt2[0], pt2[1]);
+                g2.DrawLine(new Pen(myBrush), pt2II[0], pt2II[1]);
+            }
+            else if(IV==0)
+            {
+                IV = 2;
+                pt2[0].X = 5; pt2[0].Y = 95; pt2[1].X = 55; pt2[1].Y = 145;
+                pt2II[0].X = 5; pt2II[0].Y = 145; pt2II[1].X = 55; pt2II[1].Y = 95;
+                g2.DrawLine(new Pen(myBrush), pt2[0], pt2[1]);
+                g2.DrawLine(new Pen(myBrush), pt2II[0], pt2II[1]);
+            }
+            else if (V == 0)
+            {
+                V = 2;
+                pt2[0].X = 65; pt2[0].Y = 95; pt2[1].X = 115; pt2[1].Y = 145;
+                pt2II[0].X = 65; pt2II[0].Y = 145; pt2II[1].X = 115; pt2II[1].Y = 95;
+                g2.DrawLine(new Pen(myBrush), pt2[0], pt2[1]);
+                g2.DrawLine(new Pen(myBrush), pt2II[0], pt2II[1]);
+            }
+            else if (VI == 0)
+            {
+                VI = 2;
+                pt2[0].X = 125; pt2[0].Y = 95; pt2[1].X = 175; pt2[1].Y = 145;
+                pt2II[0].X = 125; pt2II[0].Y = 145; pt2II[1].X = 175; pt2II[1].Y = 95;
+                g2.DrawLine(new Pen(myBrush), pt2[0], pt2[1]);
+                g2.DrawLine(new Pen(myBrush), pt2II[0], pt2II[1]);
+            }
+            else if (VII == 0)
+            {
+                VII = 2;
+                pt2[0].X = 5; pt2[0].Y = 155; pt2[1].X = 55; pt2[1].Y = 205;
+                pt2II[0].X = 5; pt2II[0].Y = 205; pt2II[1].X = 55; pt2II[1].Y = 155;
+                g2.DrawLine(new Pen(myBrush), pt2[0], pt2[1]);
+                g2.DrawLine(new Pen(myBrush), pt2II[0], pt2II[1]);
+            }
+            else if (VIII == 0)
+            {
+                VIII = 2;
+                pt2[0].X = 65; pt2[0].Y = 155; pt2[1].X = 115; pt2[1].Y = 205;
+                pt2II[0].X = 65; pt2II[0].Y = 205; pt2II[1].X = 115; pt2II[1].Y = 155;
+                g2.DrawLine(new Pen(myBrush), pt2[0], pt2[1]);
+                g2.DrawLine(new Pen(myBrush), pt2II[0], pt2II[1]);
+            }
+            else if (IX == 0)
+            {
+                IX = 2;
+                pt2[0].X = 125; pt2[0].Y = 155; pt2[1].X = 175; pt2[1].Y = 205;
+                pt2II[0].X = 125; pt2II[0].Y = 205; pt2II[1].X = 175; pt2II[1].Y = 155;
+                g2.DrawLine(new Pen(myBrush), pt2[0], pt2[1]);
+                g2.DrawLine(new Pen(myBrush), pt2II[0], pt2II[1]);
+            }
+
+            if (I == 2 && II == 2 && III == 2)
+            {
+                pt1[0].X = 5; pt1[0].Y = 60;
+                pt1[1].X = 175; pt1[1].Y = 60;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You lose!";
+            }
+            if (IV == 2 && V == 2 && VI == 2)
+            {
+                pt1[0].X = 5; pt1[0].Y = 120;
+                pt1[1].X = 175; pt1[1].Y = 120;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You lose!";
+            }
+            if (VII == 2 && VIII == 2 && IX == 2)
+            {
+                pt1[0].X = 5; pt1[0].Y = 180;
+                pt1[1].X = 175; pt1[1].Y = 180;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You lose!";
+            }
+            if (I == 2 && IV == 2 && VII == 2)
+            {
+                pt1[0].X = 30; pt1[0].Y = 35;
+                pt1[1].X = 30; pt1[1].Y = 205;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You lose!";
+            }
+            if (II == 2 && V == 2 && VIII == 2)
+            {
+                pt1[0].X = 90; pt1[0].Y = 35;
+                pt1[1].X = 90; pt1[1].Y = 205;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You lose!";
+            }
+            if (III == 2 && VI == 2 && IX == 2)
+            {
+                pt1[0].X = 150; pt1[0].Y = 35;
+                pt1[1].X = 150; pt1[1].Y = 205;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You lose!";
+            }
+            if (I == 2 && V == 2 && IX == 2)
+            {
+                pt1[0].X = 5; pt1[0].Y = 35;
+                pt1[1].X = 175; pt1[1].Y = 205;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You lose!";
+            }
+            if (III == 2 && V == 2 && VII == 2)
+            {
+                pt1[0].X = 175; pt1[0].Y = 35;
+                pt1[1].X = 5; pt1[1].Y = 205;
+                g2.DrawLine(new Pen(myBrush2), pt1[0], pt1[1]);
+                I = 3; II = 3; III = 3; IV = 3; V = 3; VI = 3; VII = 3; VIII = 3; IX = 3;
+                label1.Text = "You lose!";
+            }
+
+            if ((I != 0 && II != 0 && III != 0 && IV != 0 && V != 0 && VI != 0 && VII != 0 && VIII != 0 && IX != 0)
+                && (I != 3 && II != 3 && III != 3 && IV != 3 && V != 3 && VI != 3 && VII != 3 && VIII != 3 && IX != 3))
+                label1.Text = "Draw!";
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            I = 0; II = 0; III = 0; IV = 0; V = 0; VI = 0; VII = 0; VIII = 0; IX = 0;
+            Graphics g3 = this.CreateGraphics();
+            g3.Clear(this.BackColor);
+            Brush myBrush = new SolidBrush(Color.Black);
+            g3.DrawRectangle(new Pen(myBrush), 0, 30, 60, 60);
+            g3.DrawRectangle(new Pen(myBrush), 60, 30, 60, 60);
+            g3.DrawRectangle(new Pen(myBrush), 120, 30, 60, 60);
+            g3.DrawRectangle(new Pen(myBrush), 0, 90, 60, 60);
+            g3.DrawRectangle(new Pen(myBrush), 60, 90, 60, 60);
+            g3.DrawRectangle(new Pen(myBrush), 120, 90, 60, 60);
+            g3.DrawRectangle(new Pen(myBrush), 0, 150, 60, 60);
+            g3.DrawRectangle(new Pen(myBrush), 60, 150, 60, 60);
+            g3.DrawRectangle(new Pen(myBrush), 120, 150, 60, 60);
+
+            label1.Text = "    ";
+        }
+    }
+}
---
+++ Windows Programming/hw3/hw3/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+namespace hw3
+{
+    public partial class Form1 : Form
+    {
+        Color c = Color.Red;
+        List<Point> startPt = new List<Point>();
+        List<Point> endPt = new List<Point>();
+        List<Color> colorPt = new List<Color>();
+        List<bool> lineSolidPt = new List<bool>();
+        List<int> lineWidthPt = new List<int>();
+        bool lineSolid = true;
+        int lineWidth = 3;
+        Point p1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            redToolStripMenuItem.Checked = true;
+            solidToolStripMenuItem.Checked = true;
+            toolStripMenuItem4.Checked = true;
+            openFileDialog1.Filter = "線段檔(*.pnt)|*.pnt";
+            saveFileDialog1.Filter = "線段檔(*.pnt)|*.pnt";
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            c = Color.Red;
+            redToolStripMenuItem.Checked = true;
+            greenToolStripMenuItem.Checked = false;
+            blueToolStripMenuItem.Checked = false;
+        }
+
+        private void greenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            c = Color.Green;
+            redToolStripMenuItem.Checked = false;
+            greenToolStripMenuItem.Checked = true;
+            blueToolStripMenuItem.Checked = false;
+        }
+
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            c = Color.Blue;
+            redToolStripMenuItem.Checked = false;
+            greenToolStripMenuItem.Checked = false;
+            blueToolStripMenuItem.Checked = true;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            p1 = e.Location;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            startPt.Add(p1);
+            endPt.Add(e.Location);
+            colorPt.Add(c);
+            lineSolidPt.Add(lineSolid);
+            lineWidthPt.Add(lineWidth);
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            for (int i = 0; i < endPt.Count(); i++)
+            {
+                Pen p1 = new Pen(colorPt[i], lineWidthPt[i]);
+                if (lineSolidPt[i])
+                    p1.DashStyle = DashStyle.Solid;
+                else
+                    p1.DashStyle = DashStyle.Dash;
+                e.Graphics.DrawLine(p1, startPt[i], endPt[i]);
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String s = saveFileDialog1.FileName;
+                BinaryWriter outFile = new BinaryWriter(File.Open(s, FileMode.Create));
+                outFile.Write(endPt.Count());
+                for (int i = 0; i < endPt.Count(); i++)
+                {
+                    outFile.Write(startPt[i].X);
+                    outFile.Write(startPt[i].Y);
+                    outFile.Write(endPt[i].X);
+                    outFile.Write(endPt[i].Y);
+                    outFile.Write(colorPt[i].A);
+                    outFile.Write(colorPt[i].R);
+                    outFile.Write(colorPt[i].G);
+                    outFile.Write(colorPt[i].B);
+                    outFile.Write(lineSolidPt[i]);
+                    outFile.Write(lineWidthPt[i]);
+                }
+                outFile.Close();
+            }
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String s = openFileDialog1.FileName;
+                if (!File.Exists(s)) return;
+                BinaryReader inFile = new BinaryReader(File.Open(s, FileMode.Open));
+                startPt.Clear();
+                endPt.Clear();
+                int n = inFile.ReadInt32();
+                for (int i = 0; i < n; i++)
+                {
+                    startPt.Add(new Point(inFile.ReadInt32(), inFile.ReadInt32()));
+                    endPt.Add(new Point(inFile.ReadInt32(), inFile.ReadInt32()));
+                    Color nC = Color.FromArgb(inFile.ReadByte(), inFile.ReadByte(), inFile.ReadByte(), inFile.ReadByte());
+                    colorPt.Add(nC);
+                    lineSolidPt.Add(inFile.ReadBoolean());
+                    lineWidthPt.Add(inFile.ReadInt32());
+                }
+                Invalidate();
+                inFile.Close();
+            }
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            startPt.Clear();
+            endPt.Clear();
+            Invalidate();
+        }
+
+        private void solidToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lineSolid = true;
+            solidToolStripMenuItem.Checked = true;
+            dashToolStripMenuItem.Checked = false;
+            Invalidate();
+
+        }
+
+        private void dashToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lineSolid = false;
+            solidToolStripMenuItem.Checked = false;
+            dashToolStripMenuItem.Checked = true;
+            Invalidate();
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            lineWidth = 1;
+            toolStripMenuItem2.Checked = true;
+            toolStripMenuItem3.Checked = false;
+            toolStripMenuItem4.Checked = false;
+            toolStripMenuItem5.Checked = false;
+            toolStripMenuItem6.Checked = false;
+            Invalidate();
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            lineWidth = 2;
+            toolStripMenuItem2.Checked = false;
+            toolStripMenuItem3.Checked = true;
+            toolStripMenuItem4.Checked = false;
+            toolStripMenuItem5.Checked = false;
+            toolStripMenuItem6.Checked = false;
+            Invalidate();
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            lineWidth = 3;
+            toolStripMenuItem2.Checked = false;
+            toolStripMenuItem3.Checked = false;
+            toolStripMenuItem4.Checked = true;
+            toolStripMenuItem5.Checked = false;
+            toolStripMenuItem6.Checked = false;
+            Invalidate();
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            lineWidth = 4;
+            toolStripMenuItem2.Checked = false;
+            toolStripMenuItem3.Checked = false;
+            toolStripMenuItem4.Checked = false;
+            toolStripMenuItem5.Checked = true;
+            toolStripMenuItem6.Checked = false;
+            Invalidate();
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+            lineWidth = 4;
+            toolStripMenuItem2.Checked = false;
+            toolStripMenuItem3.Checked = false;
+            toolStripMenuItem4.Checked = false;
+            toolStripMenuItem5.Checked = false;
+            toolStripMenuItem6.Checked = true;
+            Invalidate();
+        }
+    }
+}
